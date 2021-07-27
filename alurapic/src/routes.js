76762a15df8486ebdf379b469764a7da1a656bf1
Gabriel Gregório Silveira Netto <silveiranetto94@gmail.com>
2021-07-27 14:00:37 -0300

    Configuração do Lazy loading e code Splitting. Bundles gerados separadamente

--- alurapic/src/routes.js
+++ alurapic/src/routes.js
@@ -1,10 +1,12 @@
-import Cadastro from './components/cadastro/Cadastro.vue';
+//import Cadastro from './components/cadastro/Cadastro.vue';
+//separa o componente Cadastro para apenas quando o usuário clica no componente e será carregado "Lazy Loading"
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue').then(m => m.default);
 import Home from './components/home/Home.vue';
 
 export const routes = [
     { path: '/home', name: 'home', component: Home, titulo: 'Home', menu: true },
     { path: '/cadastro', name: 'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true },
-    { path: '/cadastro/:id', name: 'alterar', component: Cadastro, titulo: 'Alterar'},
+    { path: '/cadastro/:id', name: 'alterar', component: Cadastro, titulo: 'Alterar' },
     { path: '*', component: Home, menu: false }//rota default da aplicação
 
 ];
