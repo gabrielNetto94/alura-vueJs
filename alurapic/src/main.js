4e3133ce451e11473ca88ba493527214d4b0809c
Gabriel Gregório Silveira Netto <silveiranetto94@gmail.com>
2021-07-27 15:49:13 -0300

    Ajustar cabeçalho da requisição

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -21,9 +21,23 @@ import './assets/js/teste.js';
 Vue.use(VueResource);
 //se a variável não estiver setada, utilizar a localhost
 Vue.http.options.root = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000';
-
 Vue.use(VueRouter);
 
+/*
+//Setar informações no cabeçalho da requisição
+Vue.http.interceptors.push((req, next) => {
+
+  // é possível colocar informações no header antes do envio da requisição
+  req.headers.set('Authorization', 'informação de segurança aqui');
+  console.log('Lidando com o request');
+
+  next(res => {
+    console.log('Lidando com a resposta')
+    // é possível acessar os dados da reposta e realizar transformações antes
+    console.log(res.body);
+  });
+});
+*/
 const router = new VueRouter({
   routes,
   mode: 'history'
